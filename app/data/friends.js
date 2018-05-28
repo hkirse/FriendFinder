e987c9c0d0566f8c641e07fbf7f037814df9fcfb
Henry Kirse <hkirse@gmail.com>
2018-05-28 19:08:08 -0400

    added home page html as well as dogs array

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+var animalArray = [
+    {
+        name: "English Bulldog",
+        photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSw4O4c8HMXlURR8Sh4RtK-xWMBeT67RRv_uD8xYzQTrr9L4_G_8g",
+        scores:[2, 1, 1, 3, 1, 4, 1, 1, 5, 1]
+    },
+    {
+        name: "Labrador Retriever",
+        photo:"https://www.google.com/search?biw=1163&bih=558&tbm=isch&sa=1&ei=EYMMW9-kDYHs5gKR7JuwDQ&q=labrador+retriever&oq=labrador+r&gs_l=img.1.0.0l10.16260.17473.0.19797.2.2.0.0.0.0.91.179.2.2.0....0...1c.1.64.img..0.2.178...0i67k1.0.BA_J8_2ijC8#imgrc=Fgf8Jjevn0lDxM:",
+        scores:[1, 1, 4, 1, 1, 1, 1, 1, 1, 3]
+    },
+    {
+        name: "Great Dane",
+        photo:"https://www.google.com/search?biw=1163&bih=558&tbm=isch&sa=1&ei=cYIMW_SAItKn5gKeqbLIAw&q=great+dane&oq=great+dane&gs_l=img.3..0i67k1l2j0j0i67k1l3j0l4.401508.402843.0.402982.10.8.0.2.2.0.97.707.8.8.0....0...1c.1.64.img..0.10.716....0.NYsuCSB99vc#imgrc=X51uQpsQAAmrxM:",
+        scores:[1, 1, 1, 3, 1, 1, 5, 1, 1, 1]
+    },
+    {
+        name: "Boxer",
+        photo:"https://www.google.com/search?biw=1163&bih=558&tbm=isch&sa=1&ei=EYMMW9-kDYHs5gKR7JuwDQ&q=boxer+dog&oq=boxer&gs_l=img.3.1.0i67k1l2j0l8.5573.6314.0.9215.5.5.0.0.0.0.95.423.5.5.0....0...1c.1.64.img..0.5.423....0.jT9WAaQZr9A#imgrc=sf8IjyQ1LkWyAM:",
+        scores:[1, 1, 1, 3, 1, 1, 5, 1, 1, 1]
+    },
+    {
+        name: "St Bernard",
+        photo: "https://www.google.com/search?biw=1163&bih=558&tbm=isch&sa=1&ei=EYMMW9-kDYHs5gKR7JuwDQ&q=st+bernard&oq=st+&gs_l=img.1.0.0i67k1l3j0l5j0i67k1l2.35628.37810.0.41735.5.3.2.0.0.0.93.264.3.3.0....0...1c.1.64.img..0.5.276....0.1zCrGiwf9Dk#imgrc=UhiRlHdWNkyDlM:",
+        scores:[1, 1, 1, 3, 1, 1, 5, 1, 1, 1]
+    },
+    {
+        name: "Common House Cat",
+        photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.mnn.com%2Fassets%2Fimages%2F2013%2F02%2Fgrumpycat.jpg.560x0_q80_crop-smart.jpg&imgrefurl=https%3A%2F%2Fwww.mnn.com%2Ffamily%2Fpets%2Fstories%2Fgrumpy-cat-isnt-so-cranky-after-all&docid=fCzN1MO73JWv1M&tbnid=sLTY8qo23N8frM%3A&vet=10ahUKEwjP17GEvanbAhXqwVkKHf-JCmMQMwhwKDMwMw..i&w=530&h=300&bih=558&biw=1163&q=grumpy%20cat&ved=0ahUKEwjP17GEvanbAhXqwVkKHf-JCmMQMwhwKDMwMw&iact=mrc&uact=8",
+        scores:[1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    }
+];
+
+
+module.exports = animalArray;
