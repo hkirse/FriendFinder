c4da9183a736ad2eb3ab937f670abe51ce1a82bd
Henry Kirse <hkirse@gmail.com>
2018-05-29 23:04:06 -0400

    json error.  trying again in the morning

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,15 +1,63 @@
 // Dependencies
 // =============================================================
-var animalArray = require('../data/friends.js');
+var animals = require('../data/friends');
+var path = require("path");
 
 module.exports = function(app) {
   // Friends GET route will be used to display a JSON of all possible friends
   app.get('/api/friends', function(req, res) {
-      res.json(animalArray);
+      res.json(animals);
   });
 
   // Friends post route will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
   app.post('/api/friends', function(req, res) {
-      animalArray.push(req.body);
+      animals.push(req.body);
+
+      console.log("new dog array:"+ animals);
+       
+      
+          console.log(animals);
+          console.log("------------------------------------");
+
+          var user = req.body;
+            
+          var userScores = user.scores;
+          console.log("userScores: " + userScores);
+        //keeps the loop from comparing the user to themselves
+        var loopNum = animals.length - 1;
+          var newAnimal = [];
+          //loops through array of potential friends
+          for (var j = 0; j < loopNum; j++){
+            var totalDifference = 0;
+                var potentialFriend = animals[j].scores;
+                console.log("potential friend scores: " + potentialFriend)
+                    //loops through potential friend's scores to compare 
+                    for (var m = 0; m < userScores.length; m++ ){
+                        totalDifference += Math.abs((userScores[m] - potentialFriend[m]));
+                    };
+                //adds difference from each potential friend to an array
+                newAnimal.push(totalDifference);
+            //end for loop
+          }
+
+    var matchNum = Math.min.apply(null, newAnimal);
+    console.log("matchNum: " +matchNum);
+    var findMatch = newAnimal.indexOf(matchNum);
+    console.log("findMatch: " +findMatch);
+    var dogName = animals[findMatch].name;
+    var dogImage = animals[findMatch].photo;
+
+    console.log("dogName: " +dogName);
+
+    var dog = {
+        name: dogName,
+        photo: dogImage
+    }
+    
+   
+    res.json(dog);
+
+        // Add current user to friendsArray
+    animals.push(req.body);
   });
 };
--- server.js
+++ server.js
@@ -7,8 +7,19 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: true }));
+// Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT: " + PORT);
+});
+
